e172294ef53ed21f32fd3e4507d7a9013bafb0f0
Troy Tran <troy.tran.goldenowl@gmail.com>
2019-10-30 22:06:06 +0700

    Updates

---
+++ precache-manifest.f433316a60fe12d0224f90298b702ff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b7baefd85fae84bd1c11f3e1ce4686",
+    "url": "/dotran150797/about-me-app/index.html"
+  },
+  {
+    "revision": "90734ee3a65300a6d0ae",
+    "url": "/dotran150797/about-me-app/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "86f3b6c5ab849d911aa5",
+    "url": "/dotran150797/about-me-app/static/js/2.130a6454.chunk.js"
+  },
+  {
+    "revision": "90734ee3a65300a6d0ae",
+    "url": "/dotran150797/about-me-app/static/js/main.5c689e2b.chunk.js"
+  },
+  {
+    "revision": "7cf1d8c2a5b4523a2e9e",
+    "url": "/dotran150797/about-me-app/static/js/runtime-main.9482e5f4.js"
+  }
+]);
